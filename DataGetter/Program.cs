6b9b748d5bd7fd98873f3bc61f41d6a85d853d42
Hayama Umi <umi@umiblog.cn>
2019-05-16 23:13:20 +0800

    init

---
+++ DataGetter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.Hosting;
+
+namespace DataGetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new HostBuilder();
+        
+            builder.Build().Run();
+        }
+    }
+}
