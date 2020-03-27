645814a248f8eed64be3b03b6d1f0010d72a2f48
Hayama Umi <umi@umiblog.cn>
2020-03-27 19:10:49 +0800

    数据库改变，表已建立

--- create.sql
+++ create.sql
@@ -9,11 +9,38 @@
 		index (`user`,`createtime`)
 ) engine=innodb charset=utf8;
 
+-- 管理者
+create table `info_admin`(
+		`id`   int UNSIGNED not null PRIMARY key AUTO_INCREMENT,
+		`account` varchar(20) not null UNIQUE, -- 用户名
+		`password` varchar(2048) not null, -- 密码(PBKDF2-HMAC-SHA512加密)
+		`phone`   varchar(20) not null -- 用来发送验证码的手机	
+)engine=innodb charset=utf8;
 
-create table `info_user`(
-		`id`  int  not null primary key auto_increment,
-    `uid` varchar(150) not null  unique key,
-    `email` varchar(100) not null unique key,
-    `comment` varchar(200) null    
-    
-) engine=innodb charset=utf8;
+-- 服务器表
+create table `info_server`(
+        `id`   int UNSIGNED not null PRIMARY key AUTO_INCREMENT,
+        `tags` varchar(50) not null unique, -- 服务器标记
+        `address` varchar(200) not null, -- 服务器地址,
+        `adminId` int UNSIGNED not null -- 管理者ID 
+)engine=innodb charset=utf8;
+
+-- 服务器用户
+create table `info_v2user`(
+		`id`   int UNSIGNED not null PRIMARY key AUTO_INCREMENT,
+		`email` varchar(200) not null, -- email
+		`account` varchar(500) not null, -- uuid
+		`alterId` int UNSIGNED not null, -- 额外ID
+		`level` int UNSIGNED not null, -- 所属级别
+		`disabled` int default 0, -- 是否禁用， 0 不禁用，1启用
+		`serverId` int UNSIGNED not null -- 所属服务器ID
+)engine=innodb charset=utf8;
+
+-- 短信验证码表
+create table `info_sms`(
+		`id`   int UNSIGNED not null PRIMARY key AUTO_INCREMENT,
+		`code` varchar(10) not null, -- 验证码
+		`used` tinyint UNSIGNED DEFAULT 0,-- 是否已经使用，0没有使用
+		`adminId` int UNSIGNED not null, -- 管理员ID
+		`ctime` timestamp not null -- 创建时间
+)engine=innodb charset=utf8;
